4355ffeed03e997276251006f6c5bfb5dc108cc6
Lionel Clément <lionel.clement@labri.fr>
2020-09-21 15:41:27 +0200

    Delete ISpriteFactory.java

--- src/src/fr/ubordeaux/ao/labyrinth/controller/ISpriteFactory.java
+++
@@ -1,9 +0,0 @@
-package fr.ubordeaux.ao.labyrinth.controller;
-
-public interface ISpriteFactory {
-	
-	ISprite create(String fileName) throws Exception;
-	ISprite create() throws Exception;
-	ISprite create(int num) throws Exception;
-
-}
